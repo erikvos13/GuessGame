4244d161cad2b1576954cbe4695e4e7e14d4ddfd
Erik Vos <erikvos13@gmail.com>
2023-12-06 22:25:27 +0100

    menu now has 2 game options pkmn and abc

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 # to do:
 # plek voor de lijst maken, maar allemaal onzichtbaar
 # guessed lijst vullen en correcte zichtbaar maken
-# mr. mime wordt bij de punt al goedgekeurd??
+# mr. mime wordt gesplitst in "mr." en "mime" ! daardoor totaal 152... komt omdat split ook op de spatie afgaat?
 # speloptie laten kiezen: moet het op volgorde ja/nee
-# speloptie laten kiezen: welke lijst wil je spelen?
 
-import pandas as pd
+
 import pygame
 import sys
 
@@ -34,18 +33,14 @@ input_rect2 = pygame.Rect(200, 300, 3, 80)
 input_rect3 = pygame.Rect(200, 380, 300, 3)
 input_rect4 = pygame.Rect(500, 300, 3, 80)
 startbtnrect = pygame.Rect(100, 100, 200, 80)
-   
+abcbtnrect = pygame.Rect(100, 200, 200, 80)   
 
-#list1 = open("Alphabet.txt").read().split()
+abclistraw = open("Alphabet.txt").read().split()
+abclist = [x.lower() for x in abclistraw]    #change to all lower case
 
-list1 = open("Pokemon.txt").read().split()
+pkmnlistraw = open("Pokemon.txt").read().split()
+pkmnlist = [x.lower() for x in pkmnlistraw]    #change to all lower case
 
-
-lclist1 = [x.lower() for x in list1]    #change to all lower case
-
-
-WINNING_SCORE = len(lclist1)
-print(WINNING_SCORE)
 
 guessed = []        #deze lijst begint leeg maar wordt gevuld met goed geraden pokemon (of uberhaupt met alles wat wordt ingevoerd)
 
@@ -59,12 +54,15 @@ def starting_menu():
     startmenu = True
     clock = pygame.time.Clock()
 
-    startbtnx1 = 100
-    startbtnx2 = 200
-    startbtny1 = 100
-    startbtny2 = 200
+    startbtnx1, startbtnx2 = 100, 300
+    startbtny1, startbtny2 = 100, 180
+
+    abcbtnx1, abcbtnx2 = 100, 300
+    abcbtny1, abcbtny2 = 200, 280
+
 
     startbtnrect
+    abcbtnrect
 
     while startmenu:
         for event in pygame.event.get():
@@ -81,7 +79,10 @@ def starting_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 m_x, m_y = pygame.mouse.get_pos()
                 if (startbtnx1 < m_x < startbtnx2) & (startbtny1 < m_y < startbtny2):
-                    main()
+                    main("pkmn")
+                elif (abcbtnx1 < m_x < abcbtnx2) & (abcbtny1 < m_y < abcbtny2):
+                    main("abc")
+
                     
         clock.tick(FPS)
         draw_menu(WIN)
@@ -95,19 +96,23 @@ def draw_menu(win):
     
     #start button
     pygame.draw.rect(win, "Green", startbtnrect)
-    startbtntext = MENUBUTTON_FONT.render("START", 1, WHITE)
+    startbtntext = MENUBUTTON_FONT.render("PKMN", 1, WHITE)
     win.blit(startbtntext, (100, 100))
+
+    #game 2 button
+    pygame.draw.rect(win, "Green", abcbtnrect)
+    abcbtn_text = MENUBUTTON_FONT.render("ABC", 1, WHITE)
+    win.blit(abcbtn_text, (100, 200))
     
     pygame.display.update()
 
-def draw(win, score, user_text, compliment):
+def draw(win, score, user_text, compliment, chosengame, winning_score):
     win.fill(BLACK)
 
     score_text = SCORE_FONT.render(f"{score}", 1, WHITE)
     win.blit(score_text, (WIDTH//4 - score_text.get_width()//2, 20))
     
-        
-    nrleft = WINNING_SCORE-score
+    nrleft = winning_score-score
     nrleft_text = SCORE_FONT.render(f"{nrleft}", 1, WHITE)
     win.blit(nrleft_text, (3 * WIDTH//4 - nrleft_text.get_width()//2, 20))
     
@@ -120,32 +125,39 @@ def draw(win, score, user_text, compliment):
     pygame.draw.rect(win, "Green", input_rect4)
     
     if compliment == True:
-        goodguess = SCORE_FONT.render("You did it!", 1, WHITE)
+        goodguess = SCORE_FONT.render("Correct!", 1, WHITE)
         WIN.blit(goodguess, (WIDTH//2 - goodguess.get_width()//2, HEIGHT//2 - goodguess.get_height()//3))
     
     pygame.display.update()
     
 
 
-def checkguess(user_text):
+def checkguess(user_text, activelist):
+
     if user_text == "Abra":
         return True
         #if in lijst & nog niet eerder genoemd
         ## if in gen1-lijst & NIET in 'al-genoemd' lijst
-
-    if user_text.lower() in lclist1:
+    
+    if user_text.lower() in activelist:
         if user_text.lower() in guessed:
             pass
         else:
             guessed.append(user_text.lower())
             return True
 
-def main():
+def main(chosengame):
+    if chosengame == "pkmn":
+        activelist = pkmnlist
+    elif chosengame == "abc":
+        activelist = abclist
+    
+    WINNING_SCORE = len(activelist)
+    
     run = True
     clock = pygame.time.Clock()
     score = 0
     user_text = ""
-    input_rect = pygame.Rect(200, 200, 140, 32)
     message_end_time = 0
     compliment = False
     
@@ -165,15 +177,14 @@ def main():
                 else:
                     user_text += event.unicode
                 
-        draw(WIN, score, user_text, compliment)
+        draw(WIN, score, user_text, compliment, chosengame, WINNING_SCORE)
         
         current_time = pygame.time.get_ticks()
         
-        if checkguess(str(user_text)) == True:
+        if checkguess(str(user_text), activelist) == True:
             message_end_time = pygame.time.get_ticks() + 2000 # display for 2 seconds
                     
             score += 1   
-            #pygame.time.wait(2000)
             user_text = ""
             
         if current_time < message_end_time:
@@ -187,15 +198,15 @@ def main():
             won = True            
 
         if won:
-            text = SCORE_FONT.render("You did it!", 1, WHITE)
+            WIN.fill(BLACK)
+            text = SCORE_FONT.render("Great job !!", 1, WHITE)
             WIN.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2))
             pygame.display.update()
-            pygame.time.delay(5000)
-            
-        
+            pygame.time.delay(4000)
+            pygame.quit()
+            sys.exit()
+                 
     
-    
-
 
 
 
